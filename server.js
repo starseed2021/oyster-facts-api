f3f5676a7b246020b2d8a257d8c7ad257d7279cb
starseed2021 <tiff.harper18@gmail.com>
2022-02-02 19:45:47 -0500

    added server

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('oysterData.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+
