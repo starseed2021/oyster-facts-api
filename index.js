d6773d4ed7d8780fa8ce410a00db59e1318b8c31
starseed2021 <tiff.harper18@gmail.com>
2022-02-03 08:25:15 -0500

    add headers in random function

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const facts = require("./oysterData.json");
 
 // RESEARCH THIS !!!
-// const headers = {
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Methods": "GET",
-//     "Access-Control-Allow-Headers": "Content-Type",
-// };
+const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET",
+    "Access-Control-Allow-Headers": "Content-Type",
+};
 
 async function handleRequest(request) {
     try {
